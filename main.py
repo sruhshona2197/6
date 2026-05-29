910d90bf47425c75e421bd5d94c720552e7386b9
sruhshona2197 <sruhshona2197@gmail.com>
2026-05-29 18:35:47 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,47 @@
+
+# 40
+class Nurse:
+    def help(self):
+        return "Helping"
+
+# 41
+class Shop:
+    def open(self):
+        return "Shop opened"
+
+# 42
+class Customer:
+    def buy(self):
+        return "Buying"
+
+# 43
+class Product:
+    def price(self):
+        return 100
+
+# 44
+class Pen:
+    color = "Blue"
+
+# 45
+class Pencil:
+    size = "Small"
+
+# 46
+class Bottle:
+    volume = "1L"
+
+# 47
+class Mobile:
+    def call(self):
+        return "Calling"
+
+# 48
+class TV:
+    def on(self):
+        return "TV ON"
+
+# 49
+class Fan:
+    def rotate(self):
+        return "Rotating"
